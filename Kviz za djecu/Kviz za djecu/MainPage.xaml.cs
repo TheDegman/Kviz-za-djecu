26eb3001b6a914da57a8113c4c215ae5bbbb3b46
The Degman <dorianmarusic@gmail.com>
2021-08-19 00:29:03 +0200

    Add project files.

---
+++ Kviz za djecu/Kviz za djecu/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Kviz_za_djecu
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async private void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new PrvoPitanje());
+        }
+
+        async private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new ONama());
+        }
+    }
+}
